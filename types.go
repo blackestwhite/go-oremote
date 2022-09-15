0097bdca340807381ee5a5279c91a091e9129d7b
blackestwhite <pesaregoal@gmail.com>
2022-09-15 19:31:39 +0430

    add types

---
+++ types.go
@@ -0,0 +1,31 @@
+package gooremote
+
+// external types
+
+// Use this struct to decode data pushed to webhook
+type WebhookPayload struct {
+	ID     string `json:"id"`
+	Amount int    `json:"amount"`
+}
+
+// internal types
+
+type instance struct {
+	Key string
+}
+
+type newPaymentResponse struct {
+	Ok               bool   `json:"ok"`
+	ErrorCode        int    `json:"error_code"`
+	ErrorDescription string `json:"error_description"`
+	Result           struct {
+		ID string `json:"id"`
+	}
+}
+
+type payment struct {
+	Amount      int    `bson:"amount" json:"amount"`
+	Next        string `bson:"next" json:"next"`
+	Webhook     string `bson:"webhook" json:"webhook"`
+	Description string `bson:"description" json:"description"`
+}
