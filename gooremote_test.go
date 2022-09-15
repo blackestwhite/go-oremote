464a8d90af7f70fceeb5befae922ac43029e2e3d
blackestwhite <pesaregoal@gmail.com>
2022-09-15 20:18:50 +0430

    Add new test

--- gooremote_test.go
+++ gooremote_test.go
@@ -26,12 +26,24 @@ func TestNewPayment(t *testing.T) {
 }
 
 func TestVerify(t *testing.T) {
+	godotenv.Load()
+	accessKey := os.Getenv("ACCESS_KEY")
+
 	i := gooremote.New("1234567890")
 	paid, err := i.Verify("fewgwrwgreg")
 	if err == nil {
 		t.Error("error should not be nil, oops: ", err)
 	}
+	if paid {
+		t.Error("paid should be false")
+	}
 
+	i = gooremote.New(accessKey)
+	id, _ := i.NewPayment(10000, "https://oremote.org", "", "test")
+	paid, err = i.Verify(id)
+	if err != nil {
+		t.Error(id, err.Error())
+	}
 	if paid {
 		t.Error("paid should be false")
 	}
