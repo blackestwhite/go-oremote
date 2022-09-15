b174ea979554e05d24a94f73743874d839e72e08
blackestwhite <pesaregoal@gmail.com>
2022-09-15 19:58:41 +0430

    Add verify test

--- gooremote_test.go
+++ gooremote_test.go
@@ -13,3 +13,15 @@ func TestNewPayment(t *testing.T) {
 		t.Error("error should not be nil, oops: ", err)
 	}
 }
+
+func TestVerify(t *testing.T) {
+	i := gooremote.New("1234567890")
+	paid, err := i.Verify("fewgwrwgreg")
+	if err == nil {
+		t.Error("error should not be nil, oops: ", err)
+	}
+
+	if paid {
+		t.Error("paid should be false")
+	}
+}
