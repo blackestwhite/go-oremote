6d159303b508831ab50196b002fd24ecdf6eae10
blackestwhite <pesaregoal@gmail.com>
2022-09-15 20:04:02 +0430

    add more tests

--- gooremote_test.go
+++ gooremote_test.go
@@ -1,16 +1,27 @@
 package gooremote_test
 
 import (
+	"os"
 	"testing"
 
 	gooremote "github.com/blackestwhite/go-oremote"
+	"github.com/joho/godotenv"
 )
 
 func TestNewPayment(t *testing.T) {
+	godotenv.Load()
+	accessKey := os.Getenv("ACCESS_KEY")
+
 	i := gooremote.New("1234567890")
 	_, err := i.NewPayment(10000, "https://oremote.org", "", "test")
 	if err == nil {
 		t.Error("error should not be nil, oops: ", err)
+	}
+
+	i = gooremote.New(accessKey)
+	_, err = i.NewPayment(10000, "https://oremote.org", "", "test")
+	if err != nil {
+		t.Error("error should be nil, err: ", err)
 	}
 }
 
